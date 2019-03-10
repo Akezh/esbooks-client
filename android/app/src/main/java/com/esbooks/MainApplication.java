d517b5a18973716693d70db330425b718e96d9dc
Akezh <rakishev.akezhan@mail.ru>
2019-03-10 13:24:23 +0600

    fix: ios libraries linked

--- android/app/src/main/java/com/esbooks/MainApplication.java
+++ android/app/src/main/java/com/esbooks/MainApplication.java
@@ -3,9 +3,9 @@ package com.esbooks;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new LinearGradientPackage(),
-            new VectorIconsPackage(),
-            new LottiePackage()
+            new VectorIconsPackage()
       );
     }
 
